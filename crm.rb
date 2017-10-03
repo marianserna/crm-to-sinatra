75852cf320af194f6d1e26cf9c7d90118de09660
Marian Serna <marianhalliday@gmail.com>
2017-10-03 16:10:38 -0400

    Adding contacts route + view & Bringing in contacts from db in order to display cotnact info 🍳

--- crm.rb
+++ crm.rb
@@ -1,7 +1,19 @@
 require_relative 'contact'
 require 'sinatra'
 
-
 get '/' do
   erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  # raise @contacts.inspect
+  erb :contacts
+end
+
+
+# MiniRecord config
+
+after do
+  ActiveRecord::Base.connection.close
 end
