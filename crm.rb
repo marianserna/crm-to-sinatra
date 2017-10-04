77e634ab3253ae3275c46319269f0ada9f78a99f
Marian Serna <marianhalliday@gmail.com>
2017-10-04 14:30:54 -0400

    Configuring contact route + /contacts/:id and single contact view

--- crm.rb
+++ crm.rb
@@ -6,19 +6,27 @@ get '/' do
   erb :index
 end
 
-# get '/contacts' do
-#   @contacts = Contact.all
-#   # raise @contacts.inspect
-#   erb :contacts
-# end
+get '/contacts' do
+  @contacts = Contact.all
+  # # raise @contacts.inspect
+  erb :contacts
+end
+
+get '/contacts/:id' do
+  # @contact = Contact.find(params[:id])
+  @contact = Contact.find_by({id: params[:id].to_i})
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 get '/about' do
   erb :about
 end
 
-
 # MiniRecord config
-
 after do
   ActiveRecord::Base.connection.close
 end
